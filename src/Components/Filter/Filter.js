42d4dda83c6564c96b7e878db26e3e652beb2761
julesntare <julesntare@gmail.com>
2022-12-04 23:01:34 +0200

    ui

--- src/Components/Filter/Filter.js
+++ src/Components/Filter/Filter.js
@@ -1,16 +1,18 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import "./Filter.scss";
 import BuildingsList from "../../assets/APIs/BuildingsList.json"
 
 
 export default function Filter(){
-    const [selectRoom, setSelectRoom] =  useState("");
-    const [selectDate, setSelectDate] =  useState("");
-    const [selectHour, setSelectHour] =  useState("");
-    const [selectCapacity, setSelectCapacity] =  useState("");
-    const time = new Date();
-  const placeholderTime = `${time.getFullYear()}-${time.getMonth()}-${time.getDate()}`;
-    
+    const [searchRoom, setSearchRoom] =  useState("");
+    const roomNameRef = useRef();
+    const roomCapacityRef = useRef();
+    const dateRef = useRef();
+    const hoursRangeRef = useRef();
+
+    const handleFilterNames = (name)=>{
+      setSearchRoom(name)
+    }
   window.onscroll = ()=> myFunction();
   const [sticky, setSticky] = useState("navbar container filter  d-flex  p-1 mt-5 w-100")
 
@@ -22,41 +24,65 @@ function myFunction() {
 
   }
 }
-    const handleSubmit = (data) =>{
-        data.preventDefault();
-        console.log(data);
-      }
-  const  roomNames = BuildingsList.map((building)=>building.rooms.map((room, i )=>(
-                <option value={selectRoom} key={i}>{room.name}</option>
-  )))
+const handleSubmit = (data) =>{
+    data.preventDefault();
+    console.log(data);
+  }
+   let roomNames;
+   let roomBySearch;
+  const defaultRooms = [];
+  BuildingsList.map((building)=>defaultRooms.push(building.rooms))
+  if(!searchRoom){
+    roomNames =defaultRooms.map((room, i )=>room.map(name=>(
+        <option  key={i}>{name.name}</option>
+      )))
+  }else if(searchRoom){
+    roomBySearch =defaultRooms.map((room, i )=>room.filter((roomName)=>{
+      return roomName.name.toLowerCase().includes(searchRoom.toLocaleLowerCase());
+    })).map((room)=>room.map((name, i)=>( 
+      <option  key={i}>{name.name}</option> 
+    )))
+  }
+
      return(
         <nav className={sticky}>
             <h3 className="lead bg-none fw-bold text-start  m-0 filter-text">Filters</h3>
             <form className="d-flex align-items-center form " role="filter" onSubmit={handleSubmit}>
-              <div className=" d-flex input-box me-2 h-100 justify-content-between align-items-center">
-                <select   id="1" onChange={setSelectRoom}   className="form-select box-filter text-dark form-select-sm p-1 me-2"  aria-label=".form-select-lg example">
-                  <option selected disabled>Room name</option>
-                    {roomNames}
-                </select>
-                <select  id="2"  onChange={setSelectCapacity} className="form-select box-filter form-select-sm p-1 me-2" aria-label=".form-select-sm example">
-                  <option selected disabled >Room Capacity</option>
-                  <option value={selectCapacity}>&lt;300</option>
-                  <option value={selectCapacity}>&lt;200</option>
-                  <option value={selectCapacity}>&lt;100</option>
-                  <option value={selectCapacity}>&lt;50</option>
-                </select>
-                <input id="3" 
-                   onChange={setSelectDate}  
-                   className="form-control form-select-sm p-1 box-filter me-2"
-                   value ={selectDate} 
-                   type="date"  aria-label="time" 
-                   />
-                <select  onChange={setSelectHour}  id="4" className="form-select form-select-sm bg-white p-1 box-filter me-2" aria-label=".form-select-sm example">
-                <option selected disabled ></option>
-                  <option value={selectHour}>08:00 - 11:00</option>
-                  <option value={selectHour}>11:00 - 02:00</option>
-                  <option value={selectHour}>02:00 - 05:00</option>
-                </select>
+              <div className=" d-flex input-box  px-1 me-2 h-100 justify-content-start w-100 align-items-center">
+                <label className="d-flex flex-column me-lg-2" htmlFor="roomName" defaultValue={"Room name"}>
+                  <div className="w-100 text-start d-flex flex-column room-name justify-content-between flex-lg-row ps-2 fw-bold">
+                    <small >Room name</small>
+                    <input onChange={(e)=>handleFilterNames(e.target.value)} className="bg-light form-control form-select-sm mx-1 autoSearch"  placeholder="Search a room"/>
+                  </div>
+                  <select   id="roomName" ref={roomNameRef}  className="form-select box-filter text-dark form-select-sm p-1 me-2"  aria-label=".form-select-lg example">
+                      {!searchRoom? roomNames : roomBySearch}
+                  </select>
+                </label>
+                <label className="d-flex flex-column me-lg-2" htmlFor="roomCapacity" defaultValue={"Room capacity"}>
+                  <small className="w-100 text-start ps-2 fw-bold">Room capacity</small>
+                  <select  id="roomCapacity"  ref={roomCapacityRef} className="form-select box-filter form-select-sm p-1 me-2" aria-label=".form-select-sm example">
+                    <option>&lt;300</option>
+                    <option>&lt;200</option> 
+                    <option>&lt;100</option>
+                    <option>&lt;50</option>
+                  </select>
+                </label>
+                <label className="d-flex flex-column me-lg-2" htmlFor="date">
+                  <small className="w-100 text-start ps-2 fw-bold">Date</small>
+                  <input id="date" 
+                    ref={dateRef}
+                    className="form-control form-select-sm p-1 box-filter me-2"
+                    type="date"  aria-label="time" 
+                    />
+                </label >
+                <label className="d-flex flex-column me-lg-2" htmlFor="hoursRange">
+                  <small className="w-100 text-start ps-2 fw-bold">Hour range</small>
+                  <select  ref={hoursRangeRef} id="hoursRange" className="form-select form-select-sm bg-white p-1 box-filter me-2" aria-label=".form-select-sm example">
+                    <option >08:00 - 11:00</option>
+                    <option >11:00 - 02:00</option>
+                    <option >02:00 - 05:00</option>
+                  </select>
+                </label>
               </div>
               {/* <button className="btn-p d-flex justify-content-center px-3 py-1 align-items-center " type="submit"><i className="bi text-primary bi-search"></i></button> */}
             </form>
